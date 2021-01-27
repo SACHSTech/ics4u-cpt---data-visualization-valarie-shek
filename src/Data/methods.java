07874c5ad2901590c28ccf8866768f9bdab494cc
Valarie Shek <valarieyuikei.shek21@ycdsbk12.ca>
2021-01-27 17:21:38 -0500

    test trial

--- src/Data/methods.java
+++ src/Data/methods.java
@@ -17,6 +17,7 @@ import javafx.stage.Stage;
 
 public class methods {
     public static void main (String[] args) throws IOException{
+        
         String fileName = "C:\\ICS4U\\ics4u-cpt---data-visualization-valarie-shek\\src\\Data\\nba_2020_advanced.csv";
         File file = new File(fileName);
 
@@ -28,8 +29,9 @@ public class methods {
 
             while(inputStream.hasNext()){
                 String line = inputStream.next();
-                String[] values = line.split(",");
-                lines.add(Arrays.asList(values));
+                System.out.println(line);
+                //String[] values = line.split(",");
+                    //lines.add(Arrays.asList(values));
             }
             inputStream.close();
         }catch (FileNotFoundException e){
@@ -40,13 +42,18 @@ public class methods {
         for(List<String> line: lines){
             int columnNo = 1;
             for(String value: line){
-                System.out.println(value);
+                //System.out.println(value);
                 //System.out.println("Line"+lineNo+"Column"+columnNo+":"+value);
                 columnNo++;
             }
             lineNo++;
         }
+        
+
+        String[][] testing1 = new String[652][26];
+
+    
         //System.out.println("{0:12}{1:>10}{2:>15}{3:>25}{4:>30}{5:>35}{6:>40}{7:>45}{8:>50}{9:>55}{10:>60}{11:>65}{12:>70}{13:>75}{14:>80}{15:>85}{16:>90}{17:>95}{18:>100}{19:>105}{20:>110}{21:>115}{22:>120}{23:>125}{24:>130}{25:>135}".format("Player","Pos","Age","Tm","G","MP","PER","TS%","3PAr","FTr","ORB%","DRB%","TRB%","AST%","STL%","BLK%","TOV%","USG%","OWS","DWS","WS","WS/48","OBPM","DBPM","BPM","VORP"));
-        //System.out.prinltn("{0:12}{1:>10}{2:>15}{3:>25}{4:>30}{5:>35}{6:>40}{7:>45}{8:>50}{9:>55}{10:>60}{11:>65}{12:>70}{13:>75}{14:>80}{15:>85}{16:>90}{17:>95}{18:>100}{19:>105}{20:>110}{21:>115}{22:>120}{23:>125}{24:>130}{25:>135}".format()
+        //System.out.prinltn("{0:12}{1:>10}{2:>15}{3:>25}{4:>30}{5:>35}{6:>40}{7:>45}{8:>50}{9:>55}{10:>60}{11:>65}{12:>70}{13:>75}{14:>80}{15:>85}{16:>90}{17:>95}{18:>100}{19:>105}{20:>110}{21:>115}{22:>120}{23:>125}{24:>130}{25:>135}".format(line)
 }
 }
