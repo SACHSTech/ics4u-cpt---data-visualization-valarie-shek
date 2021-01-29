9bfafff537639d8f8763ee32f1eacc9642ce9e2d
Valarie Shek <valarieyuikei.shek21@ycdsbk12.ca>
2021-01-29 12:39:47 -0500

    pie chart save

--- src/Data/Piechart.java
+++ src/Data/Piechart.java
@@ -2,6 +2,10 @@ package Data;
 
 import java.io.IOException;
 
+
+import javafx.scene.Parent;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,25 +18,23 @@ public class Piechart extends Application {
  
     @Override public void start(Stage stage) {
         Scene scene = new Scene(new Group());
-        stage.setTitle("Imported Fruits");
+        stage.setTitle("Age Pie Chart Representation");
         stage.setWidth(500);
         stage.setHeight(500);
  
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-                new PieChart.Data("Grapefruit", 13),
-                new PieChart.Data("Oranges", 25),
-                new PieChart.Data("Plums", 10),
-                new PieChart.Data("Pears", 22),
-                new PieChart.Data("Apples", 30));
+                new PieChart.Data("<20", 1.9),
+                new PieChart.Data("20~30", 76.9 ),
+                new PieChart.Data(">30", 21.1));
         final PieChart chart = new PieChart(pieChartData);
-        chart.setTitle("Imported Fruits");
+        chart.setTitle("Age Pie Chart Representation");
 
         ((Group) scene.getRoot()).getChildren().add(chart);
         stage.setScene(scene);
         stage.show();
     }
  
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         launch(args);
     }
 }
